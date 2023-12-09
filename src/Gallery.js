8500b03d4159f5f25e9abcc31abe6afc807e1d69
Matthew O. Smith <matthewo.smith@imail.org>
2023-12-09 08:22:28 -0700

    refactor main app and start working on a gallery

---
+++ src/Gallery.js
@@ -0,0 +1,131 @@
+import { KeyboardArrowLeft , KeyboardArrowRight} from '@mui/icons-material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+import * as React from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const default_images = [
+    {
+        label: 'San Francisco – Oakland Bay Bridge, United States',
+        imgPath:
+            'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+    },
+    {
+        label: 'Bird',
+        imgPath:
+            'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+    },
+    {
+        label: 'Bali, Indonesia',
+        imgPath:
+            'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+    },
+    {
+        label: 'Goč, Serbia',
+        imgPath:
+            'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+    },
+];
+
+function Gallery({images=default_images, onClose}) {
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = images.length;
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStepChange = (step) => {
+        setActiveStep(step);
+    };
+
+    return (
+        <Box sx={{ maxWidth: '90%', maxHeight: '90%', flexGrow: 1 }} >
+            <Paper
+                square
+                elevation={0}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '90%',
+                    pl: 2,
+                    bgcolor: 'background.default',
+                }}
+            >
+                <Typography>{images[activeStep].label}</Typography>
+            </Paper>
+            <AutoPlaySwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents
+                onClick={onClose}
+            >
+                {images.map((step, index) => (
+                    <div key={step.label}>
+                        {Math.abs(activeStep - index) <= 2 ? (
+                            <Box
+                                component="img"
+                                sx={{
+                                    height: '90%',
+                                    display: 'block',
+                                    maxWidth: '90%',
+                                    overflow: 'hidden',
+                                    width: '90%',
+                                    alignItems: 'center', 
+                                    justifyContent: 'center', 
+                                }}
+                                src={step.imgPath}
+                                alt={step.label}
+                            />
+                        ) : null}
+                    </div>
+                ))}
+            </AutoPlaySwipeableViews>
+            <MobileStepper
+                steps={maxSteps}
+                position="static"
+                activeStep={activeStep}
+                nextButton={
+                    <Button
+                        size="small"
+                        onClick={handleNext}
+                        disabled={activeStep === maxSteps - 1}
+                    >
+                        Next
+                        {theme.direction === 'rtl' ? (
+                            <KeyboardArrowLeft />
+                        ) : (
+                            <KeyboardArrowRight />
+                        )}
+                    </Button>
+                }
+                backButton={
+                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                        {theme.direction === 'rtl' ? (
+                            <KeyboardArrowRight />
+                        ) : (
+                            <KeyboardArrowLeft />
+                        )}
+                        Back
+                    </Button>
+                }
+            />
+        </Box>
+    );
+}
+
+export default Gallery;
---
+++ src/RedditApp.js
@@ -0,0 +1,195 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import RedditBox from './RedditBox';
+import RedditLayout from './RedditLayout';
+
+const RedditApp = ({ type, name, default_category }) => {
+
+    const [posts, setPosts] = useState([]);
+    // const [subreddit, setSubreddit] = useState('popular'); // Default subreddit
+    const [category, setCategory] = useState(default_category); // Default category
+    const [includeNSFW, setIncludeNSFW] = useState(false); // NSFW toggle
+    const [after, setAfter] = useState(null); // To keep track of pagination
+    const [isLoading, setIsLoading] = useState(false); // To manage loading state
+    const [loadMore, setLoadMore] = useState(true);
+    const [debouncedSubreddit, setDebouncedSubreddit] = useState(name);
+
+
+    if (name !== debouncedSubreddit) {
+        setLoadMore(false)
+        setDebouncedSubreddit(name)
+        setPosts([]);  // Reset posts
+        setAfter(null); // Reset pagination token
+
+        setIsLoading(false)
+        setLoadMore(true)
+    }
+
+    // console.log(subreddit + ' ' + debouncedSubreddit)
+
+    const debounce = (func, delay) => {
+        let inDebounce;
+        return function () {
+            const context = this;
+            const args = arguments;
+            clearTimeout(inDebounce);
+            inDebounce = setTimeout(() => func.apply(context, args), delay);
+        };
+    };
+
+    const handleSubredditChange = debounce((newSubreddit) => {
+        setDebouncedSubreddit(newSubreddit);
+    }, 500); // 500ms delay
+
+    useEffect(() => {
+        console.log("useEffect:" + loadMore + " " + isLoading)
+        if (loadMore && !isLoading) {
+            setIsLoading(true);
+            console.log(type + ' ' + name)
+            axios.get(`https://www.reddit.com/${type}/${name}/${category}.json?after=${after}&show=all`)
+                .then(response => {
+                    const newPosts = response.data.data.children
+                        .map(obj => obj.data)
+                        .filter(post => includeNSFW || !post.over_18); // Filter based on NSFW setting
+                    setPosts(prevPosts => [...prevPosts, ...newPosts]);
+                    setAfter(response.data.data.after);
+                    setIsLoading(false);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                    setIsLoading(false);
+                }).finally(() => {
+                    setLoadMore(false); // Reset loadMore flag
+                });
+        }
+    },
+        // eslint-disable-next-line 
+        [debouncedSubreddit, category, includeNSFW, after, loadMore]); // Add includeNSFW as a dependency
+
+
+
+    const newSubreddit = (s) => {
+        console.log("newSubreddit")
+        setLoadMore(false);
+        setPosts([]);
+        setAfter(null);
+        // setSubreddit(_ => s)
+        handleSubredditChange(s);
+        setIsLoading(false);
+        setLoadMore(true);
+        // Reset posts
+        // Reset pagination token
+
+    };
+
+    const handleNsfw = () => {
+        setLoadMore(false);
+        setAfter(null);
+        setPosts([])
+        setIncludeNSFW((f) => !f)
+        setIsLoading(false)
+        setLoadMore(true);
+
+
+    }
+
+    const handleCategory = (v) => {
+        setLoadMore(false);
+        setPosts([])
+        setAfter(null);
+        setCategory(v)
+        setIsLoading(false)
+        setLoadMore(true);
+
+    }
+
+    useEffect(() => {
+        const handleScroll = () => {
+            // console.log("handleScroll")
+            const scrollPosition = window.innerHeight + document.documentElement.scrollTop;
+            const threshold = document.documentElement.offsetHeight - 100; // Adjust as needed
+            // console.log("handleScroll:scrollPosition:" + scrollPosition)
+            // console.log("handleScroll:threshold:" + threshold)
+            // console.log("handleScroll:isLoading:" + isLoading)
+
+            if (scrollPosition >= threshold && !isLoading) {
+                // console.log("handleScroll:loadMore:" + loadMore)
+                setLoadMore(true);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [isLoading]); // `isLoading` is a dependency here
+
+
+
+    return (
+        <div className="App">
+            {type}
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => newSubreddit(e.target.value)}
+                placeholder="Enter subreddit"
+            />
+            <select value={category} onChange={(e) => handleCategory(e.target.value)}>
+                <option value="hot">Hot</option>
+                <option value="new">New</option>
+                <option value="rising">Rising</option>
+                <option value="top">Top</option>
+                <option value="controversial">Controversial</option>
+                <option value="submitted">Submitted</option>
+
+            </select>
+            <label>
+                Include NSFW
+                <input
+                    type="checkbox"
+                    checked={includeNSFW}
+                    onChange={handleNsfw}
+                />
+            </label>
+            <RedditLayout>
+                {posts.map(post => {
+
+
+                    //console.log(post.thumbnail, post.is_video)
+                    let thumbnail = post.thumbnail.startsWith('http') ? post.thumbnail : null
+
+                    if (!thumbnail && post.preview) {
+                        thumbnail = decodeURI(post.preview.images[0].resolutions[0].url)
+                    }
+                    if (!thumbnail && post.url_overridden_by_dest) {
+                        thumbnail = post.url_overridden_by_dest
+                    }
+                    if (post.is_video) {
+                        thumbnail = decodeURI(post.media.reddit_video.scrubber_media_url)
+                    }
+                    return <RedditBox
+                        key={post.id}
+                        id={post.id}
+                        title={post.title}
+                        author={post.author}
+                        url={post.url}
+                        domain={post.domain}
+                        selftext={post.selftext}
+                        thumbnail={thumbnail}
+                        fullImageUrl={post.url}
+                        numComments={post.num_comments}
+                        permalink={post.permalink}
+                        subreddit={post.subreddit}
+                        isNSFW={post.over_18}
+                        created_utc={post.created_utc}
+                        isSpoiler={post.spoiler}
+                        isStickied={post.stickied}
+                        isVideo={post.is_video}
+                        crosspostParent={post.crosspost_parent_list ? post.crosspost_parent_list[0] : null}
+                    />
+                })}
+            </RedditLayout>
+            {isLoading && <div>Loading more posts...</div>}
+        </div>
+    );
+};
+
+export default RedditApp;
