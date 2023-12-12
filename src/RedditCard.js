002de6828b73a724416ad930ac5d1e89012e796c
Matthew O. Smith <matt@m0smith.com>
2023-12-11 21:23:26 -0700

    Use Modal

--- src/RedditCard.js
+++ src/RedditCard.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
+
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
+
+
+import { Close } from '@mui/icons-material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 import { Link } from 'react-router-dom';
+import { CardActionArea, Dialog } from '@mui/material';
+
+
+
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -33,7 +36,16 @@ export default function RedditCard({ title, selftext, domain, isVideo, url, thum
     const [expandedComments, setExpandedComments] = React.useState(false);
     const [comments, setComments] = useState([]);
     const [showComments, setShowComments] = useState(false);
-    const [isImageModalOpen, setImageModalOpen] = useState(false);
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+        console.log("open")
+        setOpen(true);
+    }
+    const handleClose = () => {
+        console.log("close")
+        setOpen(false);
+    }
+
 
     const handleExpandClick = () => {
         setExpanded(!expanded);
@@ -60,63 +72,90 @@ export default function RedditCard({ title, selftext, domain, isVideo, url, thum
         return date.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
     };
 
-    return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardContent>
 
-                {isNSFW && <span style={{ color: 'red' }}>NSFW </span>}
-                {isSpoiler && <span style={{ color: 'orange' }}>Spoiler </span>}
-                {isStickied && <span style={{ color: 'green' }}>Sticky </span>}
-                <Link to={`/r/${subreddit}`}>/r/{subreddit}</Link>
-                {' | '}
-                <Link to={`/user/${author}`}> u/{author} </Link>
-                <p style={{ margin: '0px 0px 0px 1em', fontStyle: 'italic' }}>{formatDate(created_utc)}</p>
-            </CardContent>
-            {thumbnail &&
-                <CardMedia
-                    component="img"
-                    height="194"
-                    image={thumbnail}
-                    alt={title}
-                />}
-            {selftext &&
+
+    return (
+        <>
+            <Card sx={{ maxWidth: 345 }} onClick={handleOpen}>
                 <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        {snippet}
-                    </Typography>
+
+                    {isNSFW && <span style={{ color: 'red' }}>NSFW </span>}
+                    {isSpoiler && <span style={{ color: 'orange' }}>Spoiler </span>}
+                    {isStickied && <span style={{ color: 'green' }}>Sticky </span>}
+                    <Link to={`/r/${subreddit}`}>/r/{subreddit}</Link>
+                    {' | '}
+                    <Link to={`/user/${author}`}> u/{author} </Link>
+                    <p style={{ margin: '0px 0px 0px 1em', fontStyle: 'italic' }}>{formatDate(created_utc)}</p>
                 </CardContent>
-            }
-            {expanded_text &&
-                <CardActions disableSpacing>
-                    {numComments > 0 && 
-                    <ExpandMore
-                        expand={expanded}
-                        onClick={handleExpandClick}
-                        aria-expanded={expanded}
-                        aria-label="show more"
-                    >
-                        Comments
-                    </ExpandMore>}
-                    <ExpandMore
-                        expand={expanded}
-                        onClick={handleExpandClick}
-                        aria-expanded={expanded}
-                        aria-label="show more"
-                    >
-                        <ExpandMoreIcon />
-                    </ExpandMore>
-                </CardActions>}
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>{expanded_text}</Typography>
-                </CardContent>
-            </Collapse>
-            <Collapse in={expandedComments} timeout="auto" unmountOnExit>
-                {comments.map(comment => (
-                <CardContent>
-                    <Typography paragraph>{comment.body}</Typography>
-                </CardContent>))}
-            </Collapse>
-        </Card>
+                {thumbnail &&
+                    <CardActionArea onClick={() => console.log("CardActionArea clicked")}>
+                        <CardMedia
+                            component="img"
+                            height="194"
+                            image={thumbnail}
+                            alt={title}
+                        />
+                    </CardActionArea>}
+                {selftext &&
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            {snippet}
+                        </Typography>
+                    </CardContent>
+                }
+                {expanded_text &&
+                    <CardActions disableSpacing>
+                        {numComments > 0 &&
+                            <ExpandMore
+                                expand={expanded}
+                                onClick={handleExpandClick}
+                                aria-expanded={expanded}
+                                aria-label="show more"
+                            >
+                                Comments
+                            </ExpandMore>}
+                        <ExpandMore
+                            expand={expanded}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMoreIcon />
+                        </ExpandMore>
+                    </CardActions>}
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>{expanded_text}</Typography>
+                    </CardContent>
+                </Collapse>
+                <Collapse in={expandedComments} timeout="auto" unmountOnExit>
+                    {comments.map(comment => (
+                        <CardContent>
+                            <Typography paragraph>{comment.body}</Typography>
+                        </CardContent>))}
+                </Collapse>
+
+            </Card>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+
+            >
+                <IconButton sx={{ ml: 'auto' }} onClick={handleClose}>
+                    <Close />
+                </IconButton>
+                <Card onClick={handleClose}>
+                    <CardMedia
+                        component={isVideo ? "video" : "img"}
+                        autoPlay={isVideo}
+                        controls={isVideo}
+
+                        image={fullImageUrl || thumbnail}
+                        alt={title}
+                    />
+                </Card>
+
+            </Dialog>
+        </>
     );
 }
